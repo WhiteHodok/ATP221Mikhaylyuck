4557a6ae9bf5151b3c24630ab68b96878c38115d
WhiteHodok <balanaraus@gmail.com>
2023-12-04 00:39:54 +0600

    Lab4Web

---
+++ ATP221Mikhaylyuck/ATP221Mikhaylyuck/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for ATP221Mikhaylyuck project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("My app")
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='home')
+]
